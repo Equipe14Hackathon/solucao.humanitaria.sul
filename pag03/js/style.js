6560b0d717a93bc3818c55517540e62691dc95da
RamonVeirone <ramonempresa2001@gmail.com>
2024-10-26 17:18:04 -0300

    Update style.js

--- pag03/js/style.js
+++ pag03/js/style.js
@@ -8,7 +8,7 @@ document.getElementById('helpForm').addEventListener('submit', function(event) {
         return;
     }
     const mensagem = `Olá, meu nome é ${nome}, meu endereço é ${endereco} e estou precisando urgente de ${necessidade}.`;
-    const telefoneWhatsApp = '5598982472054';
+    const telefoneWhatsApp = '5521976311598';
     const url = `https://api.whatsapp.com/send?phone=${telefoneWhatsApp}&text=${encodeURIComponent(mensagem)}`;
     window.open(url, '_blank');
 });
